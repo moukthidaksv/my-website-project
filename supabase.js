59b398bbd3b07e95ab60f327b57e64140d0c7ce3
moukthidaksv <moukthida7@gmail.com>
2026-09-21 16:30:42 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,7 +2,7 @@
 
 const SUPABASE_URL = "https://nioicvluznlbdclegqjx.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_PUBLISHABLE_KEY = "YOUR_KEY_HERE";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
